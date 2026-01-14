8b15c6832db61767ceaf90f0609ef69bd1fcce26
kevinjrector <github@kjrlab.com>
2026-01-13 20:51:35 -0600

    Added due_date for "tasks" so user will be able to select a date the task is due.

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class Task(Base):
     id = sa.Column(sa.Integer, primary_key=True, index=True)
     title = sa.Column(sa.String(200), index=True, nullable=False)
     description = sa.Column(sa.Text, index=False)
+    due_date = sa.Column(sa.DateTime, index=True, nullable=True)
     completed = sa.Column(sa.Boolean, default=False, index=True, nullable=False)
     created_at = sa.Column(sa.DateTime, server_default=sa.func.now(), nullable=False)
     updated_at = sa.Column(sa.DateTime, server_default=sa.func.now(), nullable=False)
